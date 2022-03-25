27a8c603a406ed2371a570be10f450a59759af8c
GreenAppleDan <denibogda@gmail.com>
2022-03-25 20:24:33 +0400

    Added assets + added map as subview

---
+++ ChallengeMOIA/ChallengeMOIA/ChallengeMOIA/Common/Utility/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  ChallengeMOIA
+//
+//  Created by Denis on 25.03.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var background: UIColor { UIColor(named: #function)! }
+    static var brand: UIColor { UIColor(named: #function)! }
+    static var primaryText: UIColor { UIColor(named: #function)! }
+    static var secondaryText: UIColor { UIColor(named: #function)! }
+}
+
--- ChallengeMOIA/ChallengeMOIA/ChallengeMOIA/Scenes/Map/MapViewController.swift
+++ ChallengeMOIA/ChallengeMOIA/ChallengeMOIA/Scenes/Map/MapViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 protocol MapDisplayLogic: AnyObject { }
 
@@ -13,9 +14,30 @@ final class MapViewController: UIViewController, MapDisplayLogic {
     var interactor: MapBusinessLogic?
     var router: (MapRoutingLogic & MapDataPassing)?
     
+    private lazy var mapView: GMSMapView = {
+        let camera = GMSCameraPosition.camera(withLatitude: 53.5499242, longitude: 9.9839786, zoom: 15.0)
+        return GMSMapView(frame: view.bounds, camera: camera)
+    }()
+    
     // MARK: View lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+        configureMap()
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        mapView.frame.size = size
+    }
+    
+    // MARK: Private
+    private func configureView() {
+        view.backgroundColor = .background
+    }
+    
+    private func configureMap() {
+        view.addSubview(mapView)
+    }
+    
 }
